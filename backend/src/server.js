e6679c1086d8b0282ce55a7f6b37035ae6613ece
Khizar-cyber-dev <hafizkhizarkhizar07@gmail.com>
2025-11-03 22:10:51 +0500

    solveing the path problems.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import passport from 'passport';
-import session from 'express-session';
-import dotenv from 'dotenv';
-import authRoute from './Route/authRoute.js'
-import './lib/passport.js';
-import { connectDB } from './lib/db.js';
+import express from "express";
+import passport from "passport";
+import session from "express-session";
+import dotenv from "dotenv";
+import authRoute from "./Route/authRoute.js";
+import "./lib/passport.js";
+import { connectDB } from "./lib/db.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -15,8 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ✅ Required for passport
-app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: true }));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -25,17 +30,18 @@ const __dirname = path.dirname(__filename);
 
 app.use("/api/auth", authRoute);
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
   const clientDistPath = path.join(__dirname, "..", "..", "frontend", "dist");
   app.use(express.static(clientDistPath));
   console.log("Serving static files from:", clientDistPath);
 
- app.get("/*", (req, res) => {
+  app.get("(.*)", (req, res) => {
     res.sendFile(path.join(clientDistPath, "index.html"));
   });
 }
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   connectDB();
-  console.log("✅ Server running on port 3000");
+  console.log(`✅ Server running on port ${PORT}`);
 });
